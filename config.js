5a7488117eaf0b132225d43837de3d9fd648ec9a
Manasvi2402 <74135076+Manasvi2402@users.noreply.github.com>
2022-03-21 11:30:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBH9nF2p7I85G3nF2t_lWOxgZAG4ElcEn8",
+  authDomain: "spectagram-app-8c554.firebaseapp.com",
+  databaseURL: "https://spectagram-app-8c554-default-rtdb.firebaseio.com",
+  projectId: "spectagram-app-8c554",
+  storageBucket: "spectagram-app-8c554.appspot.com",
+  messagingSenderId: "796267315492",
+  appId: "1:796267315492:web:98ad1162a6ca14598af3b1"
+}
---
+++ screens/PostScreen.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+ export default class PostScreen extends Component{
+     render(){
+         return(
+             <View style={styles.container}>
+                 <Text>PostScreen</Text>
+
+             </View>
+         )
+     }
+ }
+ const styles = StyleSheet.create({
+container:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+},
+ })
